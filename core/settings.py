db64d6d0bcca629bcdad96f1592bb4519cda66eb
kennedyngigi4 <kennedyngigi4@gmail.com>
2026-04-13 09:19:01 +0300

    update allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -17,13 +17,19 @@ SECRET_KEY=os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [
-    "127.0.0.1", "localhost", "api.quza.co.ke", "www.api.quza.co.ke", "185.113.249.230",  "10.0.2.2"
+    "127.0.0.1", "localhost", "api.quza.co.ke", "185.113.249.230",  "10.0.2.2"
 ]
 CORS_ALLOWED_ORIGINS = [
     "https://api.quza.co.ke",
     "https://quza.co.ke",
     "https://www.quza.co.ke",
     "http://localhost:3000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.quza.co.ke",
+    "https://quza.co.ke",
+    "https://www.quza.co.ke",
 ]
 
 # Application definition
